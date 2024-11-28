444d19ba8cf274a8541204a15dd49a3bf428b53e
justege <okuregemen1@gmail.com>
2024-11-28 22:39:20 +0300

    Save

--- src/Components/Body1.tsx
+++ src/Components/Body1.tsx
@@ -1,29 +1,37 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, ChangeEvent } from "react";
 
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Input, Text, Checkbox, Select } from "@chakra-ui/react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
-
+type InputFormType = {
+  isim: string;
+  soyIsim: string;
+  yas: number;
+};
 
 export const Body1 = () => {
-
   const [popupigosteriyormu, popupgoster] = useState(false);
-  const [sayi, sayidegistir] = useState(0);
-  const icerik = useRef(0)
+
+  const {
+    control,
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputFormType>();
+
+  const onSubmit: SubmitHandler<InputFormType> = (data) => {
+
+    const yazi = data.isim + data.soyIsim + data.yas
+  
+    return console.log('');
+  };
+
   let veri1 = 0;
-
-  useEffect(() => {
-    icerik.current = icerik.current + 5  
-  })
-
-
 
   const veri1degistir = () => {
     veri1 = veri1 + 5;
-
-    icerik.current = icerik.current + 5    
   };
-
-  // Degişmediği haline = initialState
 
   return (
     <>
@@ -31,16 +39,35 @@ export const Body1 = () => {
         Popup Degistir
       </Button>
 
-      <Button onClick={() => veri1degistir()} >
+      <Button onClick={() => veri1degistir()}>veri1degistir fonksiyonu</Button>
 
-        veri1degistir fonksiyonu
-      </Button>
+      <Text>Icerik</Text>
 
-      <Text>
-        Icerik
-      </Text>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="isim"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onChange={onChange} // send value to hook form
+              value={value}
+            />
+          )}
+        />
 
-      <Box mt={4}>{icerik.current}</Box>
+        <Controller
+          control={control}
+          name="soyIsim"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onChange={onChange} // send value to hook form
+              value={value}
+            />
+          )}
+        />
+
+        <Button type="submit">Gönder Tuşu</Button>
+      </form>
 
       {popupigosteriyormu == true ? (
         <Box>Popupi Gösteriyor</Box>
--- src/Pages/SinglePage.tsx
+++ src/Pages/SinglePage.tsx
@@ -2,16 +2,13 @@ import { Box } from "@chakra-ui/react";
 import { Body1 } from "../Components/Body1";
 
 export const SinglePage = () => {
-
-  console.log('Rendered SinglePage')
+  console.log("Rendered SinglePage");
 
   // const {id, arabamarkasi, lng} = useParams<{id?: string, arabamarkasi?: string, lng?: string}>();
 
-  return (<Box>
-
-    <Body1 />
-
-   
-  </Box>)
+  return (
+    <Box>
+      <Body1 />
+    </Box>
+  );
 };
-
